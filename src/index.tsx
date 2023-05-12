e2c5b4a931bf7b76de5ea1c75759028072431f3f
Vitalii <vitaliy.petrenko95@gmail.com>
2023-05-12 11:32:53 +0300

    change router

--- src/index.tsx
+++ src/index.tsx
@@ -4,15 +4,15 @@ import './index.css';
 import App from './components/application/App';
 import { Provider } from 'react-redux';
 import { store } from './store';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Provider store={store}>
         <App />
       </Provider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
